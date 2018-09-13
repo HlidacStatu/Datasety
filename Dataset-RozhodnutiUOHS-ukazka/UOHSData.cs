7e2eca04de404217b3fc804e3d7a39515ed0662b
Michal Blaha <michal@michalblaha.cz>
2018-09-13 17:44:20 +0200

    Dataset-RozhodnutiUOHS-ukazka
    Ukázková aplikace na volání Dataset API v C# na konkrétním příkladě rozhodnutí UOHS

---
+++ Dataset-RozhodnutiUOHS-ukazka/UOHSData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dataset_RozhodnutiUOHS
+{
+    public class UOHSData
+    {
+        public class Ucastnik
+        {
+            public string Jmeno { get; set; }
+            public string ICO { get; set; }
+        }
+        public class Dokument
+        {
+            public string Url { get; set; }
+            public string PlainText { get; set; }
+        }
+        public string Id { get; set; }
+        public string Url { get; set; }
+        public string Cj { get; set; }
+        public string Instance { get; set; }
+        public Ucastnik[] Ucastnici { get; set; }
+        public string Typ_spravniho_rizeni { get; set; }
+        public string Typ_rozhodnuti { get; set; }
+        public string Rok { get; set; }
+        public DateTime? PravniMoc { get; set; }
+        public string[] SouvisejiciUrl { get; set; }
+        public Dokument Rozhodnuti { get; set; }
+        public DateTime? Uverejneno { get; set; }
+        public string Vec { get; set; }
+    }
+}
