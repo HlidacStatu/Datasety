4a7b9469c4fcc173c587a9ad82167a749283cdde
Michal Blaha <michal@michalblaha.cz>
2019-10-09 20:27:26 +0200

    download fix
    + kml export

--- MostyRSD/Program.cs
+++ MostyRSD/Program.cs
@@ -1,11 +1,12 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using HlidacStatu.Api.Dataset.Connector;
+using Newtonsoft.Json.Linq;
+using SharpKml.Dom;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Text.RegularExpressions;
-
-using HlidacStatu.Api.Dataset.Connector; //use component from https://github.com/HlidacStatu/API/tree/master/DatasetConnector
-
+using System.Threading.Tasks;
 
 namespace MostyRSD
 {
@@ -14,13 +15,13 @@ namespace MostyRSD
         static HlidacStatu.Api.Dataset.Connector.DatasetConnector dsc;
         static void Main(string[] args)
         {
-            dsc = new HlidacStatu.Api.Dataset.Connector.DatasetConnector(
+            dsc = new HlidacStatu.Api.Dataset.Connector.DatasetConnector( 
                 System.Configuration.ConfigurationManager.AppSettings["apikey"]
                 );
 
-        //create dataset
+            //create dataset
             var dsDef = new HlidacStatu.Api.Dataset.Connector.Dataset<Most>(
-                "Stav Mostů v ČR", "Stav-Mostu", "http://bms.clevera.cz/Public", "Stav mostů v ČR", "",
+                "Stav Mostů v ČR", "Stav-Mostu", "http://bms.clevera.cz/Public", "Stav mostů v ČR. V tuto chvíli na dálnicích a silnicích I.třídy, které spravuje ŘSD.", "https://github.com/HlidacStatu/Datasety/tree/master/MostyRSD",
                 false, true,
                 new string[,] { { "Stav mostů", "Stav" }, { "Poslední kontrola", "PosledniProhlidka" } },
                 new ClassicTemplate.ClassicSearchResultTemplate()
@@ -41,14 +42,15 @@ namespace MostyRSD
                 );
 
 
-           // var datasetId = dsc.CreateDataset<Most>(dsDef).Result;
+            //var datasetId = dsc.CreateDataset<Most>(dsDef).Result;
 
             //use this later for updating dataset
             //var datasetId = dsc.UpdateDataset<Most>(dsDef).Result;
 
 
             //download, parse and save data into dataset
-            DownloadData(dsDef, "stav-mostu");
+            // DownloadData(dsDef, "stav-mostu");
+            GenerateAllKML();
         }
 
         static void DownloadData(Dataset<Most> ds, string datasetId)
@@ -61,16 +63,33 @@ namespace MostyRSD
 
             //httpClient.DefaultRequestHeaders.Add("Content-Type", "application/json;charset=UTF-8");
 
-            var content = new System.Net.Http.StringContent("{\"bounds\":{\"epsg\":\"5514\",\"esri\":\"10267\",\"xmin\":-971863.6,\"xmax\":-79136.4,\"ymin\":-1235189.7,\"ymax\":-916165},\"layers\":\" Most\",\"zoomIndex\":7}"
-                ,System.Text.Encoding.UTF8, "application/json");
-            var jsonResult = httpClient.PostAsync("http://bms.clevera.cz/api/assetregistermap/GetMapAllObjects?t=1&d=1", content)
+
+            Console.Write($"Reading data from source ....");
+
+            var content = new System.Net.Http.StringContent(
+                "{\"bounds\":{\"epsg\":\"5514\",\"esri\":\"10267\",\"xmin\":-991863.6,\"xmax\":-77136.4,\"ymin\":-1255189.7,\"ymax\":-896165},\"layers\":\" Most\",\"zoomIndex\":17}" //cela cr
+                                                                                                                                                                                      //"{\"bounds\":{\"epsg\":\"5514\",\"esri\":\"10267\",\"xmin\":-727484.2,\"xmax\":-720530.9,\"ymin\":-1065980.9,\"ymax\":-1063864.3},\"layers\":\"Most Podjezd\",\"zoomIndex\":14}" //test mirosovice
+                , System.Text.Encoding.UTF8, "application/json");
+            var jsonResult = httpClient.PostAsync("http://bms.clevera.cz/api/assetregistermap/GetMapAllObjects?t=1&d=0", content)
                         .Result.Content
                         .ReadAsStringAsync().Result;
 
+            Console.WriteLine($"Done.");
+
             var data = Newtonsoft.Json.Linq.JObject.Parse(jsonResult);
+            //JArray mosty = data["MapObjects"];
+
             List<Most> mosty = new List<Most>();
-            foreach (var jo in data["MapObjects"])
+            int count = 0;
+            int total = data["MapObjects"].Count();
+
+            ParallelOptions po = new ParallelOptions() { MaxDegreeOfParallelism = 5 };
+
+            Parallel.ForEach<JToken>(data["MapObjects"], po, jo =>
             {
+                var c = System.Threading.Interlocked.Increment(ref count);
+                if (c % 20 == 0)
+                    Console.WriteLine($"{count} z {total}");
                 Most m = new Most();
                 m.Id = jo["g"].Value<string>();
                 var gps = Geo.JTSK.ToWgs(
@@ -122,11 +141,12 @@ namespace MostyRSD
                              System.Globalization.DateTimeStyles.AssumeLocal, out var datum))
                             m.PosledniProhlidka = datum;
                     }
-                    Console.WriteLine(m.Jmeno);
+
                     var id = dsc.AddItemToDataset<Most>(ds, m).Result;
+
                 }
 
-            }
+            });
 
         }
 
@@ -143,6 +163,100 @@ namespace MostyRSD
                 }
             }
             return string.Empty;
+        }
+
+        static void GenerateAllKML()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                GenerateKML(i);
+            }
+        }
+            static void GenerateKML(int stav)
+        {
+
+            int page = 1;
+            SearchResult<Most> res = null;
+            List<Most> data = new List<Most>();
+            do
+            {
+                Console.WriteLine($"stav {stav} page {page}");
+                res = dsc.SearchItemsInDataset<Most>("stav-mostu", "Stav:" + stav, page).Result;
+                page++;
+                data.AddRange(res.results);
+
+                //break;
+            } while ((res?.results?.Count() ?? 0) > 0);
+
+            Kml kml = new Kml();
+
+            var style0 = new Style();
+            style0.Id = "stav0"; style0.Icon = new IconStyle();
+            style0.Icon.Color = new SharpKml.Base.Color32(255, 00, 200, 0); //yell
+            style0.Icon.ColorMode = ColorMode.Normal; style0.Icon.Scale = 1.0;
+
+            var style5 = new Style();
+            style5.Id = "stav5"; style5.Icon = new IconStyle();
+            style5.Icon.Color = new SharpKml.Base.Color32(255, 66, 191, 244); //yell
+            style5.Icon.ColorMode = ColorMode.Normal; style5.Icon.Scale = 1.0;
+
+            var style6 = new Style();
+            style6.Id = "stav6"; style6.Icon = new IconStyle();
+            style6.Icon.Color = new SharpKml.Base.Color32(255, 66, 134, 244); //ORAnGE
+            style6.Icon.ColorMode = ColorMode.Normal; style6.Icon.Scale = 1.0;
+
+            var style7 = new Style();
+            style7.Id = "stav7"; style7.Icon = new IconStyle();
+            style7.Icon.Color = new SharpKml.Base.Color32(255, 0, 0, 255); //red
+            style7.Icon.ColorMode = ColorMode.Normal; style7.Icon.Scale = 1.0;
+
+            var document = new Document();
+            document.AddStyle(style0); document.AddStyle(style5);
+            document.AddStyle(style6); document.AddStyle(style7);
+
+            var fdata = new SharpKml.Dom.Folder();
+            fdata.Name = "Stav " + stav;
+            foreach (var m in data.Where(m => m.Stav == stav))
+            {
+
+                Point point = new Point();
+                point.Coordinate = new SharpKml.Base.Vector(m.GPS_Lat, m.GPS_Lng);
+
+                Placemark placemark = new Placemark();
+                placemark.Id = m.Id;
+                placemark.Name = m.Jmeno;
+                placemark.Description = new Description() { Text = $"{m.MistniNazev}. Stav {m.PopisStavu}. Poslední kontrola: {m.ProhlidkaPopis}" };
+
+                var style = "#stav";
+                if (m.Stav > 4)
+                    style = style + m.Stav;
+                else
+                    style = style + "0";
+
+                placemark.StyleUrl = new Uri(style, UriKind.Relative);
+                placemark.ExtendedData = new ExtendedData();
+                placemark.ExtendedData.AddData(new Data() { Value = m.MistniNazev, Name = "Mistni nazev" });
+                placemark.ExtendedData.AddData(new Data() { Value = m.Oznaceni, Name = "Označení" });
+                placemark.ExtendedData.AddData(new Data() { Value = m.PopisStavu, Name = "Stav" });
+                placemark.ExtendedData.AddData(new Data() { Value = m.ProhlidkaPopis, Name = "Poslední prohlídka" });
+                placemark.ExtendedData.AddData(new Data() { Value = m.SpravaOrganizace, Name = "Spravující organizace" });
+                placemark.ExtendedData.AddData(new Data() { Value = m.SpravaProvozniUsek, Name = "Spravující provozní úsek" });
+                placemark.ExtendedData.AddData(new Data() { Value = m.SpravaStredisko, Name = "Spravující středisko" });
+                placemark.Geometry = point;
+                fdata.AddFeature(placemark);
+                //fdata.AddChild(placemark);
+            }
+
+            document.AddFeature(fdata);
+
+
+
+            kml.Feature = document;
+            //kml.AddChild(document);
+            SharpKml.Base.Serializer serializer = new SharpKml.Base.Serializer();
+            serializer.Serialize(kml);
+            System.IO.File.WriteAllText($"mosty_{stav}.kml", serializer.Xml);
+
         }
 
     }
