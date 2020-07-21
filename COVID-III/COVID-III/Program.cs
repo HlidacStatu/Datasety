7efd0448ffce16392ab2d80c8fc9f553bcdc3df9
Michal <michal@michalblaha.cz>
2020-07-22 00:07:18 +0200

    COVID-III preparation

---
+++ COVID-III/COVID-III/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COVID_III
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
