6f381fb2d58558f9accfee89a5526e47c177d809
Michal <michal@michalblaha.cz>
2020-12-04 20:23:01 +0100

    updates

--- Jednání Rady České televize/JednaniRadyCT/Program.cs
+++ Jednání Rady České televize/JednaniRadyCT/Program.cs
@@ -30,15 +30,15 @@ namespace JednaniRadyCT
         static bool skips2t = false;
         static void Main(string[] arguments)
         {
-            Console.WriteLine("Jednání Rady ČT - SpeechToText");
+            Console.WriteLine($"Jednání-Rady-ČT - {System.Reflection.Assembly.GetEntryAssembly().GetName().Version}");
+            Devmasters.Logging.Logger.Root.Info($"Jednání-Rady-ČT - {System.Reflection.Assembly.GetEntryAssembly().GetName().Version}");
             Devmasters.Logging.Logger.Root.Debug("Jednání Rady ČT starting with " + string.Join(',', arguments));
-            args = arguments
-                .Select(m => m.Split('='))
-                .ToDictionary(m => m[0].ToLower(), v => v.Length == 1 ? "" : v[1]);
 
 
+            var args = new Devmasters.Args(arguments);
 
-            if (args.Count < 3)
+
+            if (args.Count() < 3)
             {
                 Help(); return;
             }
@@ -46,7 +46,7 @@ namespace JednaniRadyCT
             s2t_username = args["/s2tu"];
             s2t_password = args["/s2tp"];
 
-            if (args.ContainsKey("/utdl"))
+            if (args.Exists("/utdl"))
                 YTDL = args["/utdl"];
             else
                 YTDL = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + "\\youtube-dl.exe";
@@ -54,12 +54,12 @@ namespace JednaniRadyCT
             startPath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
 
             apiKey = args["/apikey"];
-            rewrite = args.ContainsKey("/rewrite");
-            afterDay = args.ContainsKey("/daysback") ? DateTime.Now.Date.AddDays(-1 * Convert.ToInt32(args["/daysback"])) : DateTime.MinValue;
-            if (args.ContainsKey("/ids"))
-                ids = args["/ids"].Split(',', ';');
-            skips2t = args.ContainsKey("/skips2t");
+            rewrite = args.Exists("/rewrite");
+            afterDay = DateTime.Now.Date.AddDays(-1 * args.GetNumber("/daysback",10000).Value);
+                ids = args.GetArray("/ids");
+            skips2t = args.Exists("/skips2t");
 
+            int threads = args.GetNumber("/t") ?? 5;
 
             try
             {
@@ -145,10 +145,10 @@ namespace JednaniRadyCT
                         var fullJ = ParseJednani(jednani.First(m => m.Id == id));
 
                         Devmasters.Logging.Logger.Root.Debug($"Saving {id} ");
-                        ds.AddOrUpdateItem(fullJ, HlidacStatu.Api.V2.Dataset.Typed.ItemInsertMode.merge);
+                        ds.AddOrUpdateItem(fullJ, HlidacStatu.Api.V2.Dataset.Typed.ItemInsertMode.rewrite);
                     }
                     return new Devmasters.Batch.ActionOutputData() { Log = id };
-                }, true, maxDegreeOfParallelism: 5);
+                }, true, maxDegreeOfParallelism: threads);
 
         }
 
@@ -176,10 +176,14 @@ namespace JednaniRadyCT
 
             //download video/audio
             string fnFile = $"{startPath}\\wav\\{j.Id}";
-            var fnmp3 = $"{fnFile}.mp3";
-            var rawext = ".raw_s2t";
+            var MP3Fn = $"{fnFile}.mp3";
+            var newtonFn = $"{fnFile}.mp3.raw_s2t";
+            var dockerFn = $"{fnFile}.ctm";
 
-            if (System.IO.File.Exists(fnmp3 + rawext) == false && System.IO.File.Exists(fnFile) == false)
+            bool exists_mp3 = System.IO.File.Exists(MP3Fn);
+            bool exists_S2T = System.IO.File.Exists(newtonFn) || System.IO.File.Exists(dockerFn);
+
+            if (exists_mp3 == false && exists_S2T == false)
             {
                 System.Diagnostics.ProcessStartInfo pi = new System.Diagnostics.ProcessStartInfo(YTDL, $"--no-progress --extract-audio --audio-format mp3 --audio-quality 3 -o \"{fnFile}.%(ext)s\" " + j.Odkaz);
                 pi.WorkingDirectory = System.IO.Path.GetDirectoryName(YTDL);
@@ -190,152 +194,162 @@ namespace JednaniRadyCT
                 pe.Start();
             }
 
-            if (System.IO.File.Exists(fnmp3 + rawext) == false)
+            if (exists_S2T == false)
             {
-                if (skips2t == false)
+                if (skips2t)
                     return null;
 
                 var s2t_id = "ntx.v2t.engine.EngineService/cz/t-broadcast/v2t";
                 var s2t_label = "vad+v2t+ppc+pnc";
-                var s2t = new Newton.SpeechToText.Cloud.FileAPI.VoiceToText(fnmp3, s2t_username, s2t_password, s2t_id, s2t_label);
+                var s2t = new Newton.SpeechToText.Cloud.FileAPI.VoiceToTerms(MP3Fn, s2t_username, s2t_password, s2t_id, s2t_label);
                 Devmasters.Logging.Logger.Root.Info($"Starting SpeechToText for {j.Id} ");
                 s2t.Convert();
-                System.IO.File.WriteAllText(fnmp3 + rawext, s2t.Raw);
+                System.IO.File.WriteAllText(newtonFn, s2t.Raw);                
             }
-            var res = new Newton.SpeechToText.Cloud.FileAPI.VoiceToText(System.IO.File.ReadAllText(fnmp3 + rawext))
-                .TextWithTimestamps(TimeSpan.FromSeconds(10), true)
-                .Select(t => new Jednani.Blok() { SekundOdZacatku = (long)t.Start.TotalSeconds, Text = t.Text })
-                .ToArray();
 
+            Jednani.Blok[] res = null;
+            if (System.IO.File.Exists(newtonFn))
+            {
+                var tt = new Newton.SpeechToText.Cloud.FileAPI.VoiceToTerms(System.IO.File.ReadAllText(newtonFn));
+                res = new Devmasters.SpeechToText.VoiceToTextFormatter(tt.Terms)
+                   .TextWithTimestamps(TimeSpan.FromSeconds(10), true)
+                   .Select(t => new Jednani.Blok() { SekundOdZacatku = (long)t.Start.TotalSeconds, Text = t.Text })
+                   .ToArray();
+            }
+            else if (System.IO.File.Exists(dockerFn))
+            {
+                var tt = new KaldiASR.SpeechToText.VoiceToTerms(System.IO.File.ReadAllText(dockerFn));
+                res = new Devmasters.SpeechToText.VoiceToTextFormatter(tt.Terms)
+                   .TextWithTimestamps(TimeSpan.FromSeconds(10), true)
+                   .Select(t => new Jednani.Blok() { SekundOdZacatku = (long)t.Start.TotalSeconds, Text = t.Text })
+                   .ToArray();
+            }
             return res;
         }
-            else
-                return null;
-        }
 
-    static string hzapis = null;
-    private static Jednani.Dokument[] GetZapisy(Jednani j)
-    {
-        if (hzapis == null)
+        static string hzapis = null;
+        private static Jednani.Dokument[] GetZapisy(Jednani j)
         {
-            using (Devmasters.Net.HttpClient.URLContent nzapis = new Devmasters.Net.HttpClient.URLContent("https://www.ceskatelevize.cz/rada-ct/zapisy-z-jednani/"))
+            if (hzapis == null)
             {
-                hzapis = nzapis.GetContent().Text;
-            }
-        }
-        var dzapis = new Devmasters.XPath(hzapis);
-
-        var zapisy = dzapis.GetNodes("//a[@class='pdf']");
-
-        List<Jednani.Dokument> docs = new List<Jednani.Dokument>();
-        foreach (var z in zapisy)
-        {
-            if (z.InnerText.Contains($"({j.DatumJednani.ToString("d. M. yyyy")})"))
-            {
-                Uri? url = null;
-                Uri.TryCreate(new Uri(urlPrefix), z.Attributes["href"].Value, out url);
-                docs.Add(new Jednani.Dokument()
+                using (Devmasters.Net.HttpClient.URLContent nzapis = new Devmasters.Net.HttpClient.URLContent("https://www.ceskatelevize.cz/rada-ct/zapisy-z-jednani/"))
                 {
-                    HsProcessType = "document",
-                    DocumentUrl = url.AbsoluteUri,
-                    Nazev = z.InnerText.Trim(),
-                    Typ = "zápis"
-                });
-            }
-        }
-
-        return docs.ToArray();
-    }
-
-
-
-    static string mzapis = null;
-    private static Jednani.Dokument[] GetMaterialy(Jednani j)
-    {
-        if (mzapis == null)
-        {
-            using (Devmasters.Net.HttpClient.URLContent nzapis = new Devmasters.Net.HttpClient.URLContent("https://www.ceskatelevize.cz/rada-ct/materialy-projednane-radou/"))
-            {
-                mzapis = nzapis.GetContent().Text;
-            }
-        }
-        var dzapis = new Devmasters.XPath(mzapis);
-
-        var casti = dzapis.GetNodes("//div[contains(@class,'contentArticle')]/h4[@class='odsazeni']");
-
-        List<Jednani.Dokument> docs = new List<Jednani.Dokument>();
-        foreach (var z in casti)
-        {
-            if (z.InnerText.Contains($"{j.DatumJednani.ToString("d. M. yyyy")}"))
-            {
-                var pars = Devmasters.XPath.Tools.GetNodes(z, "following::*");
-                //jdi az do dalsiho h4
-                foreach (var par in pars)
-                {
-                    if (par.Name == "p")
-                    {
-                        var link = par.ChildNodes.Where(m => m.Name == "a").FirstOrDefault();
-                        if (link != null)
-                        {
-                            Uri? url = null;
-                            Uri.TryCreate(new Uri(urlPrefix), link.Attributes["href"].Value, out url);
-                            docs.Add(new Jednani.Dokument()
-                            {
-                                HsProcessType = "document",
-                                DocumentUrl = url.AbsoluteUri,
-                                Typ = "material",
-                                Nazev = link.InnerText
-                            });
-
-                        }
-                    }
-                    if (par.Name == "h4" && par.Attributes.FirstOrDefault()?.Value == "odsazeni")
-                        goto end; //dalsi h4, pryc
+                    hzapis = nzapis.GetContent().Text;
                 }
-
             }
+            var dzapis = new Devmasters.XPath(hzapis);
+
+            var zapisy = dzapis.GetNodes("//a[@class='pdf']");
+
+            List<Jednani.Dokument> docs = new List<Jednani.Dokument>();
+            foreach (var z in zapisy)
+            {
+                if (z.InnerText.Contains($"({j.DatumJednani.ToString("d. M. yyyy")})"))
+                {
+                    Uri? url = null;
+                    Uri.TryCreate(new Uri(urlPrefix), z.Attributes["href"].Value, out url);
+                    docs.Add(new Jednani.Dokument()
+                    {
+                        HsProcessType = "document",
+                        DocumentUrl = url.AbsoluteUri,
+                        Nazev = z.InnerText.Trim(),
+                        Typ = "zápis"
+                    });
+                }
+            }
+
+            return docs.ToArray();
         }
-    end:
-        return docs.ToArray();
-    }
 
 
-    public static void Help()
-    {
-        Console.WriteLine("\n" +
-            "/utdl=[FullPathTo] - cesta k youtube-dl\n" +
-            "/token=[Hlidac API token]\n" +
-            "/s2tu - newtown api login\n" +
-            "/s2tp - newtown api password]\n" +
-            "/rewrite \n" +
-            "/ids={id,id,id,...} - specific ids\n" +
-            "/skips2t - skip speech to text" +
-            "");
-    }
 
-    static HlidacStatu.Api.V2.CoreApi.Model.Registration Registration()
-    {
-        var jsonGen = new JSchemaGenerator
+        static string mzapis = null;
+        private static Jednani.Dokument[] GetMaterialy(Jednani j)
         {
-            DefaultRequired = Required.Default
-        };
-        var genJsonSchema = jsonGen.Generate(typeof(Jednani)).ToString();
+            if (mzapis == null)
+            {
+                using (Devmasters.Net.HttpClient.URLContent nzapis = new Devmasters.Net.HttpClient.URLContent("https://www.ceskatelevize.cz/rada-ct/materialy-projednane-radou/"))
+                {
+                    mzapis = nzapis.GetContent().Text;
+                }
+            }
+            var dzapis = new Devmasters.XPath(mzapis);
 
-        HlidacStatu.Api.V2.CoreApi.Model.Registration reg = new HlidacStatu.Api.V2.CoreApi.Model.Registration(
-            "Jednání Rady České televize", DataSetId,
-            "https://www.ceskatelevize.cz/rada-ct/prenos-jednani-po-internetu/",
-            "https://github.com/HlidacStatu/Datasety/tree/master/deMinimis/Jednání%20Rady%20České%20televize",
-            "Jednání, zápisy, materiály a přepis audiozáznamů jednání Rad České televize.",
-            genJsonSchema, betaversion: true, allowWriteAccess: false,
-            orderList: new string[,] {
+            var casti = dzapis.GetNodes("//div[contains(@class,'contentArticle')]/h4[@class='odsazeni']");
+
+            List<Jednani.Dokument> docs = new List<Jednani.Dokument>();
+            foreach (var z in casti)
+            {
+                if (z.InnerText.Contains($"{j.DatumJednani.ToString("d. M. yyyy")}"))
+                {
+                    var pars = Devmasters.XPath.Tools.GetNodes(z, "following::*");
+                    //jdi az do dalsiho h4
+                    foreach (var par in pars)
+                    {
+                        if (par.Name == "p")
+                        {
+                            var link = par.ChildNodes.Where(m => m.Name == "a").FirstOrDefault();
+                            if (link != null)
+                            {
+                                Uri? url = null;
+                                Uri.TryCreate(new Uri(urlPrefix), link.Attributes["href"].Value, out url);
+                                docs.Add(new Jednani.Dokument()
+                                {
+                                    HsProcessType = "document",
+                                    DocumentUrl = url.AbsoluteUri,
+                                    Typ = "material",
+                                    Nazev = link.InnerText
+                                });
+
+                            }
+                        }
+                        if (par.Name == "h4" && par.Attributes.FirstOrDefault()?.Value == "odsazeni")
+                            goto end; //dalsi h4, pryc
+                    }
+
+                }
+            }
+        end:
+            return docs.ToArray();
+        }
+
+
+        public static void Help()
+        {
+            Console.WriteLine("\n" +
+                "/utdl=[FullPathTo] - cesta k youtube-dl\n" +
+                "/token=[Hlidac API token]\n" +
+                "/s2tu - newtown api login\n" +
+                "/s2tp - newtown api password]\n" +
+                "/rewrite \n" +
+                "/ids={id,id,id,...} - specific ids\n" +
+                "/skips2t - skip speech to text" +
+                "");
+        }
+
+        static HlidacStatu.Api.V2.CoreApi.Model.Registration Registration()
+        {
+            var jsonGen = new JSchemaGenerator
+            {
+                DefaultRequired = Required.Default
+            };
+            var genJsonSchema = jsonGen.Generate(typeof(Jednani)).ToString();
+
+            HlidacStatu.Api.V2.CoreApi.Model.Registration reg = new HlidacStatu.Api.V2.CoreApi.Model.Registration(
+                "Jednání Rady České televize", DataSetId,
+                "https://www.ceskatelevize.cz/rada-ct/prenos-jednani-po-internetu/",
+                "https://github.com/HlidacStatu/Datasety/tree/master/deMinimis/Jednání%20Rady%20České%20televize",
+                "Jednání, zápisy, materiály a přepis audiozáznamů jednání Rad České televize.",
+                genJsonSchema, betaversion: true, allowWriteAccess: false,
+                orderList: new string[,] {
                     { "Podle datumu konání", "DatumJednani" },
-            },
-            defaultOrderBy: "DatumJednani desc",
-            searchResultTemplate: new ClassicTemplate.ClassicSearchResultTemplate()
-                .AddColumn("Detail", @"<a href=""{{ fn_DatasetItemUrl item.Id }}"">{{item.Titulek}}</a>")
-                .AddColumn("Datum jednání", "{{ fn_FormatDate item.DatumJednani }}")
-                .AddColumn("Délka", "{{item.Delka}}&nbsp;min")
-                .AddColumn("Materiály", @"
+                },
+                defaultOrderBy: "DatumJednani desc",
+                searchResultTemplate: new ClassicTemplate.ClassicSearchResultTemplate()
+                    .AddColumn("Detail", @"<a href=""{{ fn_DatasetItemUrl item.Id }}"">{{item.Titulek}}</a>")
+                    .AddColumn("Datum jednání", "{{ fn_FormatDate item.DatumJednani }}")
+                    .AddColumn("Délka", "{{item.Delka}}&nbsp;min")
+                    .AddColumn("Materiály", @"
 {{ if item.Zapisy && item.Zapisy.size > 0  }}
     {{ for doc in item.Zapisy }}
           {{ doc.Nazev }},
@@ -346,12 +360,12 @@ namespace JednaniRadyCT
 {{end}}
 
 ")
-            ,
-            detailTemplate: new ClassicTemplate.ClassicDetailTemplate()
-                .AddColumn("ID jednání", @"{{item.Id}}")
-                .AddColumn("Jednání", "{{item.Titulek }}")
-                .AddColumn("Datum jednání", "{{item.DatumJednani}}")
-                .AddColumn("Zápis", @"
+                ,
+                detailTemplate: new ClassicTemplate.ClassicDetailTemplate()
+                    .AddColumn("ID jednání", @"{{item.Id}}")
+                    .AddColumn("Jednání", "{{item.Titulek }}")
+                    .AddColumn("Datum jednání", "{{item.DatumJednani}}")
+                    .AddColumn("Zápis", @"
 {{ if item.Zapisy && item.Zapisy.size > 0  }}
     <ul>
     {{ for doc in item.Zapisy }}
@@ -364,7 +378,7 @@ namespace JednaniRadyCT
     </ul>
 {{end}}
 ")
-                .AddColumn("Projednávané materiály", @"
+                    .AddColumn("Projednávané materiály", @"
 {{ if item.Materialy && item.Materialy.size > 0  }}
     <ul>
     {{ for doc in item.Materialy }}
@@ -377,14 +391,14 @@ namespace JednaniRadyCT
     </ul>
 {{end}}
 ")
-                .AddColumn("Odkaz na audio", "{{item.Odkaz }}")
-                .AddColumn("Délka audio záznamu", "{{ item.Delka }} min")
-            );
+                    .AddColumn("Odkaz na audio", "{{item.Odkaz }}")
+                    .AddColumn("Délka audio záznamu", "{{ item.Delka }} min")
+                );
 
 
-        return reg;
+            return reg;
+
+        }
 
     }
-
-}
 }
