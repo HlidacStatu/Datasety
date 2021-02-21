a0086fcd36530651345a7bd83dbcc7921674c621
Michal <michal@michalblaha.cz>
2021-02-21 13:24:06 +0100

    Kapacita jednotlivych nemocnic

---
+++ Kapacita1Nemocnice/Nemocnice.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CsvHelper;
+using CsvHelper.Configuration;
+using CsvHelper.TypeConversion;
+
+using Nest;
+
+using Newtonsoft.Json.Linq;
+
+namespace Kapacita1Nemocnice
+{
+
+
+
+    public class NemocniceMap : ClassMap<Nemocnice>
+    {
+        public NemocniceMap()
+        {
+            AutoMap(System.Globalization.CultureInfo.CurrentCulture);
+            Map(m => m.id).Ignore();
+            Map(m => m.reprofilizovana_kapacita_luzka_aro_jip_kapacita_volna).Optional();
+            Map(m => m.reprofilizovana_kapacita_luzka_aro_jip_kapacita_celkem).Optional();
+            Map(m => m.reprofilizovana_kapacita_luzka_aro_jip_kapacita_planovana).Optional();
+            Map(m => m.reprofilizovana_kapacita_luzka_standard_kyslik_kapacita_volna).Optional();
+            Map(m => m.reprofilizovana_kapacita_luzka_standard_kyslik_kapacita_celkem).Optional();
+            Map(m => m.reprofilizovana_kapacita_luzka_standard_kyslik_kapacita_planovana).Optional();
+            Map(m => m.datum).Convert(row => DateTime.ParseExact(row.Row.GetField<string>(0), "yyyy-MM-dd", System.Globalization.CultureInfo.CurrentCulture));
+        }
+    }
+    public class Nemocnice
+    {
+        string _id = "";
+        [CsvHelper.Configuration.Attributes.Ignore]
+        public string id
+        {
+            get
+            {
+                return $"{datum:yyyy-MM-dd}-{zz_id}";
+            }
+        }
+
+        [Description("Datum")]
+        public DateTime datum { get; set; }
+
+        public string zz_id
+        {
+            get
+            {
+                return $"{zz_kod}-{Devmasters.Crypto.Hash.ComputeHashToHex(zz_nazev).Substring(0,8)}";
+            }
+        }
+
+        [Description("Identifikátor zdravotnického zařízení (IČZ).")]
+        public string zz_kod { get; set; }
+
+        [Description("Název zdravotnického zařízení")]
+        public string zz_nazev { get; set; }
+
+        [Description("Identifikátor kraje podle klasifikace NUTS 3.")]
+        public string kraj_nuts_kod { get; set; }
+
+        [Description("Název kraje")]
+        public string kraj_nazev { get; set; }
+
+        [Description("Počet přístrojů ECMO na pracovišti ARO A JIP (ARO – zdravotní péče z těchto lůžek se vykazuje pod odborností 7I8 nebo 7T8; JIP – zdravotní péče z těchto lůžek se vykazuje pod odborností I nebo T, nejedná se o lůžka následné a dlouhodobé péče). Volná kapacita.")]
+        public int ecmo_kapacita_volna { get; set; }
+
+        [Description("Počet přístrojů ECMO na pracovišti ARO A JIP (ARO – zdravotní péče z těchto lůžek se vykazuje pod odborností 7I8 nebo 7T8; JIP – zdravotní péče z těchto lůžek se vykazuje pod odborností I nebo T, nejedná se o lůžka následné a dlouhodobé péče). Celková kapacita.")]
+        public int ecmo_kapacita_celkem { get; set; }
+
+        [Description("Počet přístrojů UPV na pracovišti ARO A JIP (ARO – zdravotní péče z těchto lůžek se vykazuje pod odborností 7I8 nebo 7T8; JIP – zdravotní péče z těchto lůžek se vykazuje pod odborností I nebo T, nejedná se o lůžka následné a dlouhodobé péče). Volná kapacita.")]
+        public int upv_kapacita_volna { get; set; }
+
+        [Description("Počet přístrojů UPV na pracovišti ARO A JIP (ARO – zdravotní péče z těchto lůžek se vykazuje pod odborností 7I8 nebo 7T8; JIP – zdravotní péče z těchto lůžek se vykazuje pod odborností I nebo T, nejedná se o lůžka následné a dlouhodobé péče). Celková kapacita.")]
+        public int upv_kapacita_celkem { get; set; }
+
+        [Description("Počet přístrojů pro kontinuální dialýzu (bez rozlišení, na kterém pracovišti se momentálně nachází). Volná kapacita.")]
+        public int crrt_kapacita_volna { get; set; }
+
+        [Description("Počet přístrojů pro kontinuální dialýzu (bez rozlišení, na kterém pracovišti se momentálně nachází). Celková kapacita.")]
+        public int crrt_kapacita_celkem { get; set; }
+
+        [Description("Počet přístrojů pro intermitentní dialýzu (bez rozlišení, na kterém pracovišti se momentálně nachází). Nejedná se o přístroje pro léčbu pacientů s chronickým dialyzačním programem. Volná kapacita.")]
+        public int ihd_kapacita_volna { get; set; }
+
+        [Description("Počet přístrojů pro intermitentní dialýzu (bez rozlišení, na kterém pracovišti se momentálně nachází). Nejedná se o přístroje pro léčbu pacientů s chronickým dialyzačním programem. Celková kapacita.")]
+        public int ihd_kapacita_celkem { get; set; }
+
+        [Description("Počet lůžek anesteziologicko–resuscitačního oddělení (zdravotní péče z těchto lůžek se vykazuje pod odborností 7I8 nebo 7T8) a počet lůžek na jednotkách intenzivní péče pro dospělé (zdravotní péče z těchto lůžek se vykazuje pod odborností I nebo T, nejedná se o lůžka následné a dlouhodobé péče). Celková kapacita.")]
+        public int luzka_aro_jip_kapacita_celkem { get; set; }
+
+        [Description("Počet lůžek anesteziologicko–resuscitačního oddělení (zdravotní péče z těchto lůžek se vykazuje pod odborností 7I8 nebo 7T8) a počet lůžek na jednotkách intenzivní péče pro dospělé (zdravotní péče z těchto lůžek se vykazuje pod odborností I nebo T, nejedná se o lůžka následné a dlouhodobé péče). Volná kapacita pro pacienty COVID-19 pozitivní.")]
+        public int luzka_aro_jip_kapacita_volna_covid_pozitivni { get; set; }
+
+        [Description("Počet lůžek anesteziologicko–resuscitačního oddělení (zdravotní péče z těchto lůžek se vykazuje pod odborností 7I8 nebo 7T8) a počet lůžek na jednotkách intenzivní péče pro dospělé (zdravotní péče z těchto lůžek se vykazuje pod odborností I nebo T, nejedná se o lůžka následné a dlouhodobé péče). Volná kapacita pro pacienty COVID-19 negativní.")]
+        public int luzka_aro_jip_kapacita_volna_covid_negativni { get; set; }
+
+        [Description("Z celkového počtu standardních akutních lůžek se uvádí počet lůžek, ke kterým je přiveden kyslík (zdravotní péče z těchto lůžek se vykazuje pod odborností H nebo F, nejedná se o lůžka následné a dlouhodobé péče). Celková kapacita.")]
+        public int luzka_standard_kyslik_kapacita_celkem { get; set; }
+
+        [Description("Z celkového počtu standardních akutních lůžek se uvádí počet lůžek, ke kterým je přiveden kyslík (zdravotní péče z těchto lůžek se vykazuje pod odborností H nebo F, nejedná se o lůžka následné a dlouhodobé péče). Volná kapacita pro pacienty COVID-19 pozitivní.")]
+        public int luzka_standard_kyslik_kapacita_volna_covid_pozitivni { get; set; }
+
+        [Description("Z celkového počtu standardních akutních lůžek se uvádí počet lůžek, ke kterým je přiveden kyslík (zdravotní péče z těchto lůžek se vykazuje pod odborností H nebo F, nejedná se o lůžka následné a dlouhodobé péče). Volná kapacita pro pacienty COVID-19 negativní.")]
+        public int luzka_standard_kyslik_kapacita_volna_covid_negativni { get; set; }
+
+        [Description("Počet přenosných/transportních ventilátorů. Volná kapacita.")]
+        public int ventilatory_prenosne_kapacita_volna { get; set; }
+
+        [Description("Počet přenosných/transportních ventilátorů. Celková kapacita.")]
+        public int ventilatory_prenosne_kapacita_celkem { get; set; }
+
+        [Description("Počet ventilátorů na operačních sálech (přístroje, které jsou fyzicky umístěny na sálech). Volná kapacita.")]
+        public int ventilatory_operacni_sal_kapacita_volna { get; set; }
+
+        [Description("Počet ventilátorů na operačních sálech (přístroje, které jsou fyzicky umístěny na sálech). Celková kapacita.")]
+        public int ventilatory_operacni_sal_kapacita_celkem { get; set; }
+
+        [Description("Lůžka IP (UPV +/- ) nově vyhrazená pro COVID pozitivní pacienty, která byla původně určena k poskytování jiného typu péče nebo péče jiného oboru/odbornosti. Volná kapacita.")]
+        public int reprofilizovana_kapacita_luzka_aro_jip_kapacita_volna { get; set; }
+
+        [Description("Lůžka IP (UPV +/- ) nově vyhrazená pro COVID pozitivní pacienty, která byla původně určena k poskytování jiného typu péče nebo péče jiného oboru/odbornosti. Celková kapacita.")]
+        public int reprofilizovana_kapacita_luzka_aro_jip_kapacita_celkem { get; set; }
+
+        [Description("Lůžka IP (UPV +/- ) nově vyhrazená pro COVID pozitivní pacienty, která byla původně určena k poskytování jiného typu péče nebo péče jiného oboru/odbornosti. Plánovaná kapacita je pevně stanovená MZ ČR, kterou nelze v systému DIP uživatelsky měnit.")]
+        public int reprofilizovana_kapacita_luzka_aro_jip_kapacita_planovana { get; set; }
+
+        [Description("Lůžka standardní s kyslíkem nově vyhrazená pro COVID pozitivní pacienty, která byla původně určena k poskytování jiného typu péče nebo péče jiného oboru/odbornosti. Volná kapacita.")]
+        public int reprofilizovana_kapacita_luzka_standard_kyslik_kapacita_volna { get; set; }
+
+        [Description("Lůžka standardní s kyslíkem nově vyhrazená pro COVID pozitivní pacienty, která byla původně určena k poskytování jiného typu péče nebo péče jiného oboru/odbornosti. Celková kapacita.")]
+        public int reprofilizovana_kapacita_luzka_standard_kyslik_kapacita_celkem { get; set; }
+
+        [Description("Lůžka standardní s kyslíkem nově vyhrazená pro COVID pozitivní pacienty, která byla původně určena k poskytování jiného typu péče nebo péče jiného oboru/odbornosti. Plánovaná kapacita je pevně stanovená MZ ČR, kterou nelze v systému DIP uživatelsky měnit.")]
+        public int reprofilizovana_kapacita_luzka_standard_kyslik_kapacita_planovana { get; set; }
+
+        public Nemocnice() { }
+
+
+        public static Nemocnice Diff(Nemocnice fh, Nemocnice lh)
+        {
+            Nemocnice h = new Nemocnice();
+            h.datum = new DateTime((lh.datum - fh.datum).Ticks);
+            h.zz_kod = fh.zz_kod;
+            h.zz_nazev = fh.zz_nazev;
+            h.kraj_nazev = fh.kraj_nazev;
+            h.kraj_nuts_kod = fh.kraj_nuts_kod;
+            h.luzka_aro_jip_kapacita_celkem = lh.luzka_aro_jip_kapacita_celkem - fh.luzka_aro_jip_kapacita_celkem;
+            h.luzka_aro_jip_kapacita_volna_covid_pozitivni = lh.luzka_aro_jip_kapacita_volna_covid_pozitivni - fh.luzka_aro_jip_kapacita_volna_covid_pozitivni;
+            h.luzka_aro_jip_kapacita_volna_covid_negativni = lh.luzka_aro_jip_kapacita_volna_covid_negativni - fh.luzka_aro_jip_kapacita_volna_covid_negativni;
+            h.crrt_kapacita_celkem = lh.crrt_kapacita_celkem - fh.crrt_kapacita_celkem;
+            h.crrt_kapacita_volna = lh.crrt_kapacita_volna - fh.crrt_kapacita_volna;
+            h.ecmo_kapacita_celkem = lh.ecmo_kapacita_celkem - fh.ecmo_kapacita_celkem;
+            h.ecmo_kapacita_volna = lh.ecmo_kapacita_volna - fh.ecmo_kapacita_volna;
+
+            h.ihd_kapacita_celkem = lh.ihd_kapacita_celkem - fh.ihd_kapacita_celkem;
+            h.ihd_kapacita_volna = lh.ihd_kapacita_volna - fh.ihd_kapacita_volna;
+            h.reprofilizovana_kapacita_luzka_aro_jip_kapacita_celkem = lh.reprofilizovana_kapacita_luzka_aro_jip_kapacita_celkem - fh.reprofilizovana_kapacita_luzka_aro_jip_kapacita_celkem;
+            h.reprofilizovana_kapacita_luzka_aro_jip_kapacita_planovana = lh.reprofilizovana_kapacita_luzka_aro_jip_kapacita_planovana - fh.reprofilizovana_kapacita_luzka_aro_jip_kapacita_planovana;
+            h.reprofilizovana_kapacita_luzka_aro_jip_kapacita_volna = lh.reprofilizovana_kapacita_luzka_aro_jip_kapacita_volna - fh.reprofilizovana_kapacita_luzka_aro_jip_kapacita_volna;
+
+            h.reprofilizovana_kapacita_luzka_standard_kyslik_kapacita_celkem = lh.reprofilizovana_kapacita_luzka_standard_kyslik_kapacita_celkem - fh.reprofilizovana_kapacita_luzka_standard_kyslik_kapacita_celkem;
+            h.reprofilizovana_kapacita_luzka_standard_kyslik_kapacita_planovana = lh.reprofilizovana_kapacita_luzka_standard_kyslik_kapacita_planovana - fh.reprofilizovana_kapacita_luzka_standard_kyslik_kapacita_planovana;
+            h.reprofilizovana_kapacita_luzka_standard_kyslik_kapacita_volna = lh.reprofilizovana_kapacita_luzka_standard_kyslik_kapacita_volna - fh.reprofilizovana_kapacita_luzka_standard_kyslik_kapacita_volna;
+
+            h.luzka_standard_kyslik_kapacita_celkem = lh.luzka_standard_kyslik_kapacita_celkem - fh.luzka_standard_kyslik_kapacita_celkem;
+            h.luzka_standard_kyslik_kapacita_volna_covid_negativni = lh.luzka_standard_kyslik_kapacita_volna_covid_negativni - fh.luzka_standard_kyslik_kapacita_volna_covid_negativni;
+            h.luzka_standard_kyslik_kapacita_volna_covid_pozitivni = lh.luzka_standard_kyslik_kapacita_volna_covid_pozitivni - fh.luzka_standard_kyslik_kapacita_volna_covid_pozitivni;
+
+            h.upv_kapacita_celkem = lh.upv_kapacita_celkem - fh.upv_kapacita_celkem;
+            h.upv_kapacita_volna = lh.upv_kapacita_volna - fh.upv_kapacita_volna;
+            h.ventilatory_operacni_sal_kapacita_celkem = lh.ventilatory_operacni_sal_kapacita_celkem - fh.ventilatory_operacni_sal_kapacita_celkem;
+            h.ventilatory_operacni_sal_kapacita_volna = lh.ventilatory_operacni_sal_kapacita_volna - fh.ventilatory_operacni_sal_kapacita_volna;
+            h.ventilatory_prenosne_kapacita_celkem = lh.ventilatory_prenosne_kapacita_celkem - fh.ventilatory_prenosne_kapacita_celkem;
+            h.ventilatory_prenosne_kapacita_volna = lh.ventilatory_prenosne_kapacita_volna - fh.ventilatory_prenosne_kapacita_volna;
+
+
+
+            return h;
+        }
+
+        public static DateTime? ToDateTime(string value, params string[] formats)
+        {
+            if (string.IsNullOrEmpty(value))
+                return null;
+            foreach (var f in formats)
+            {
+                var dt = ToDateTime(value, f);
+                if (dt.HasValue)
+                    return dt;
+            }
+            return null;
+        }
+
+        public static DateTime? ToDateTime(string value, string format)
+        {
+            if (string.IsNullOrEmpty(value))
+                return null;
+
+            DateTime tmp;
+            if (DateTime.TryParseExact(value, format, System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.AssumeLocal | System.Globalization.DateTimeStyles.AllowWhiteSpaces, out tmp))
+                return new DateTime?(tmp);
+            else
+                return null;
+        }
+
+
+
+
+    }
+}
---
+++ Kapacita1Nemocnice/Program.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net.Http;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+using CsvHelper;
+
+using HlidacStatu.Api.V2.CoreApi.Client;
+using HlidacStatu.Api.V2.Dataset;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Schema.Generation;
+
+using OfficeOpenXml;
+
+namespace Kapacita1Nemocnice
+{
+    class Program
+    {
+
+        static HlidacStatu.Api.V2.Dataset.Typed.Dataset<Nemocnice> ds = null;
+        const int everyMins = 58;
+        public static string GetExecutingDirectoryName()
+        {
+            var location = new Uri(Assembly.GetEntryAssembly().GetName().CodeBase);
+            return new System.IO.FileInfo(location.AbsolutePath).Directory.FullName;
+        }
+        static void Main(string[] args)
+        {
+
+            var DArgs = args
+                .Select(m => m.Split('='))
+                .ToDictionary(m => m[0].ToLower(), v => v.Length == 1 ? "" : v[1]);
+
+            CreateDataset(DArgs);
+
+            //find xls url
+            string openDataPage = "https://dip.mzcr.cz/api/v1/kapacity-intenzivni-pece-zdravotnicke-zarizeni.csv";
+            Uri xlsUrl = null;
+            Devmasters.Logging.Logger.Root.Info("Getting URL of csv from " + openDataPage);
+            using (Devmasters.Net.HttpClient.URLContent net = new Devmasters.Net.HttpClient.URLContent(openDataPage))
+            {
+                Devmasters.Logging.Logger.Root.Info("Getting csv");
+                var fn = "kapacity-intenzivni-pece-zdravotnicke-zarizeni.csv";
+                System.IO.File.WriteAllBytes(fn, net.GetBinary().Binary);
+
+                using (var reader = new StreamReader(fn) )//new StringReader(html))
+                {
+                    using (var csv = new CsvReader(reader, new CsvHelper.Configuration.CsvConfiguration(System.Globalization.CultureInfo.InvariantCulture)
+                    {
+                        Delimiter = ",",
+                        IgnoreBlankLines = true,
+                        HasHeaderRecord = true,
+                        TrimOptions = CsvHelper.Configuration.TrimOptions.Trim
+                    }))
+                    {
+                        //csv.Context.RegisterClassMap<NemocniceMap>();
+                        //var records = csv.GetRecords<Nemocnice>().ToArray();
+
+                        List<Nemocnice> recs = new List<Nemocnice>();
+                        if (true)
+                        {
+                            csv.Read();
+                            csv.ReadHeader();
+                            while (csv.Read())
+                            {
+                                var record = new Nemocnice
+                                {
+                                    crrt_kapacita_celkem = csv.GetField<int>("crrt_kapacita_celkem"),
+                                    crrt_kapacita_volna = csv.GetField<int>("crrt_kapacita_volna"),
+                                    ecmo_kapacita_celkem = csv.GetField<int>("ecmo_kapacita_celkem"),
+                                    ecmo_kapacita_volna = csv.GetField<int>("ecmo_kapacita_volna"),
+                                    ihd_kapacita_celkem = csv.GetField<int>("ihd_kapacita_celkem"),
+                                    ihd_kapacita_volna = csv.GetField<int>("ihd_kapacita_volna"),
+                                    kraj_nazev = csv.GetField<string>("kraj_nazev"),
+                                    kraj_nuts_kod = csv.GetField<string>("kraj_nuts_kod"),
+                                    luzka_aro_jip_kapacita_celkem = csv.GetField<int>("luzka_aro_jip_kapacita_celkem"),
+                                    luzka_aro_jip_kapacita_volna_covid_negativni = csv.GetField<int>("luzka_aro_jip_kapacita_volna_covid_negativni"),
+                                    luzka_aro_jip_kapacita_volna_covid_pozitivni = csv.GetField<int>("luzka_aro_jip_kapacita_volna_covid_pozitivni"),
+                                    luzka_standard_kyslik_kapacita_celkem = csv.GetField<int>("luzka_standard_kyslik_kapacita_celkem"),
+                                    luzka_standard_kyslik_kapacita_volna_covid_negativni = csv.GetField<int>("luzka_standard_kyslik_kapacita_volna_covid_negativni"),
+                                    luzka_standard_kyslik_kapacita_volna_covid_pozitivni = csv.GetField<int>("luzka_standard_kyslik_kapacita_volna_covid_pozitivni"),
+                                    reprofilizovana_kapacita_luzka_aro_jip_kapacita_celkem = csv.GetField<int?>("reprofilizovana_kapacita_luzka_aro_jip_kapacita_celkem") ?? 0,
+                                    reprofilizovana_kapacita_luzka_aro_jip_kapacita_planovana = csv.GetField<int?>("reprofilizovana_kapacita_luzka_aro_jip_kapacita_planovana") ?? 0,
+                                    reprofilizovana_kapacita_luzka_aro_jip_kapacita_volna = csv.GetField<int?>("reprofilizovana_kapacita_luzka_aro_jip_kapacita_volna") ?? 0,
+                                    reprofilizovana_kapacita_luzka_standard_kyslik_kapacita_celkem = csv.GetField<int?>("reprofilizovana_kapacita_luzka_standard_kyslik_kapacita_celkem") ?? 0,
+                                    reprofilizovana_kapacita_luzka_standard_kyslik_kapacita_planovana = csv.GetField<int?>("reprofilizovana_kapacita_luzka_standard_kyslik_kapacita_planovana") ?? 0,
+                                    reprofilizovana_kapacita_luzka_standard_kyslik_kapacita_volna = csv.GetField<int?>("reprofilizovana_kapacita_luzka_standard_kyslik_kapacita_volna") ?? 0,
+                                    upv_kapacita_celkem = csv.GetField<int>("upv_kapacita_celkem"),
+                                    upv_kapacita_volna = csv.GetField<int>("upv_kapacita_volna"),
+                                    ventilatory_operacni_sal_kapacita_celkem = csv.GetField<int>("ventilatory_operacni_sal_kapacita_celkem"),
+                                    ventilatory_operacni_sal_kapacita_volna = csv.GetField<int>("ventilatory_operacni_sal_kapacita_volna"),
+                                    ventilatory_prenosne_kapacita_celkem = csv.GetField<int>("ventilatory_prenosne_kapacita_celkem"),
+                                    ventilatory_prenosne_kapacita_volna = csv.GetField<int>("ventilatory_prenosne_kapacita_volna"),
+                                    zz_kod = csv.GetField<string>("zz_kod"),
+                                    zz_nazev = csv.GetField<string>("zz_nazev"),
+                                    datum = DateTime.ParseExact(csv.GetField<string>("datum"), "yyyy-MM-dd", System.Globalization.CultureInfo.CurrentCulture)
+                                };
+                                var iis = record.id;
+                                recs.Add(record);
+                            }
+                        }
+                        //ds.AddOrRewriteItems(recs);
+                        Devmasters.Batch.Manager.DoActionForAll<Nemocnice>(recs.OrderByDescending(o=>o.datum),
+                            n =>
+                            {
+                                Console.Write(".");
+                                ds.AddOrUpdateItem(n, HlidacStatu.Api.V2.Dataset.Typed.ItemInsertMode.skip);
+
+                                return new Devmasters.Batch.ActionOutputData();
+                            },false);
+
+                    }
+                }
+            }
+        }
+
+        static void CreateDataset(Dictionary<string, string> args)
+        {
+
+            var jsonGen = new JSchemaGenerator
+            {
+                DefaultRequired = Required.Default
+            };
+            var genJsonSchema = jsonGen.Generate(typeof(Nemocnice)).ToString();
+
+            HlidacStatu.Api.V2.CoreApi.Model.Registration reg = new HlidacStatu.Api.V2.CoreApi.Model.Registration(
+                "Online dispečink intenzivní péče – volné kapacity nemocnic", "kapacity-jedn-nemocnic", "https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19", "", "Všechna data jsou přehledně zobrazena na https://www.hlidacstatu.cz/kapacitanemocnic",
+                genJsonSchema, betaversion: false, allowWriteAccess: false,
+
+                searchResultTemplate: new HlidacStatu.Api.V2.CoreApi.Model.Template("",
+                @"<br/>
+<br/>
+<br/>
+<br/>
+<p style=""font-size:160%;font-weight:bold;"">
+Všechna data jsou přehledně zobrazena na <a href=""https://www.hlidacstatu.cz/kapacitanemocnic?utm_source=dataset&utm_medium=link&utm_campaign=kapacitanemocnic"">https://www.hlidacstatu.cz/kapacitanemocnic</a>
+</p>
+<br/>
+
+<br/>
+
+Zde jsou uložena v databázi a jsou tak snadno dostupná pro veřejnost přes <a href=""https://www.hlidacstatu.cz/api/v2/swagger/index#/Datasety"">API, aktuální a ve strojově čitelné podobě</a>.
+<br/>
+<br/>
+
+Originální data v Excelu jsou ke stažení na <a href=""https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19"">https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19</a>, část ""<i>COVID‑19: Online dispečink intenzivní péče – volné kapacity
+</i>""
+<br/>
+"),
+                detailTemplate: new HlidacStatu.Api.V2.CoreApi.Model.Template("",
+                @"<br/>
+<br/>
+<br/>
+<br/>
+<p style=""font-size:160%;font-weight:bold;"">
+Všechna data jsou přehledně zobrazena na <a href=""https://www.hlidacstatu.cz/kapacitanemocnic?utm_source=dataset&utm_medium=link&utm_campaign=kapacitanemocnic"">https://www.hlidacstatu.cz/kapacitanemocnic</a>
+</p>
+<br/>
+
+<br/>
+
+Zde jsou uložena v databázi a jsou tak snadno dostupná pro veřejnost přes <a href=""https://www.hlidacstatu.cz/api/v2/swagger/index#/Datasety"">API, aktuální a ve strojově čitelné podobě</a>.
+<br/>
+<br/>
+
+Originální data v Excelu jsou ke stažení na <a href=""https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19"">https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19</a>, část ""<i>COVID‑19: Online dispečink intenzivní péče – volné kapacity
+</i>""
+<br/>
+")
+                );
+
+            try
+            {
+                ds = HlidacStatu.Api.V2.Dataset.Typed.Dataset<Nemocnice>.OpenDataset(args["/apikey"], "kapacity-jedn-nemocnic");
+                if (ds == null)
+                    ds = HlidacStatu.Api.V2.Dataset.Typed.Dataset<Nemocnice>.CreateDataset(args["/apikey"], reg);
+
+            }
+            catch (ApiException e)
+            {
+                ds = HlidacStatu.Api.V2.Dataset.Typed.Dataset<Nemocnice>.CreateDataset(args["/apikey"], reg);
+
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+
+
+        }
+
+        public static void SendMail(string to, string subject, string body, string replyTo)
+        {
+            string from = "kapacitanemocnic_app@hlidacstatu.cz";
+
+            using (var smtp = new System.Net.Mail.SmtpClient())
+            {
+                System.Net.Mail.MailMessage msg = new System.Net.Mail.MailMessage(from, to);
+                msg.Subject = subject;
+                if (!string.IsNullOrEmpty(replyTo) && Devmasters.TextUtil.IsValidEmail(replyTo))
+                    msg.ReplyToList.Add(new System.Net.Mail.MailAddress(replyTo));
+                msg.IsBodyHtml = false;
+                msg.BodyEncoding = System.Text.Encoding.UTF8;
+                msg.SubjectEncoding = System.Text.Encoding.UTF8;
+                msg.Body = body;
+
+                smtp.Send(msg);
+            }
+        }
+        public static string GetRegexGroupValue(string txt, string regex, string groupname)
+        {
+            if (string.IsNullOrEmpty(txt))
+                return null;
+            Regex myRegex = new Regex(regex, RegexOptions.IgnoreCase | RegexOptions.Multiline | RegexOptions.IgnorePatternWhitespace | RegexOptions.CultureInvariant);
+            foreach (Match match in myRegex.Matches(txt))
+            {
+                if (match.Success)
+                {
+                    if (match.Groups[groupname].Captures.Count > 1)
+                        return match.Groups[groupname].Captures[0].Value;
+                    else
+                        return match.Groups[groupname].Value;
+                }
+            }
+            return string.Empty;
+        }
+
+    }
+}
