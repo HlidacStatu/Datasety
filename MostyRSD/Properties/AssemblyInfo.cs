78c2dfecfe1782da0055b9fa20d2131adbd0a2c2
Michal Blaha <michal@michalblaha.cz>
2025-06-28 17:19:49 +0200

    Migrate project to SDK-style and .NET 8
    Converted MostyRSD.csproj to SDK-style format targeting net8.0, replacing explicit assembly and reference management with PackageReferences. Assembly metadata and versioning are now handled in the project file, and redundant attributes were removed from AssemblyInfo.cs.

--- MostyRSD/Properties/AssemblyInfo.cs
+++ MostyRSD/Properties/AssemblyInfo.cs
@@ -1,16 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("MostyRSD")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MostyRSD")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,16 +11,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("0b313521-dd9f-40d6-a86a-8a36fbee4568")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
